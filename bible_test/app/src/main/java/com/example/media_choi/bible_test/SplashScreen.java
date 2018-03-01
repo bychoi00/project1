73f7bb2f38f2f450e4c8556ce250755bd910cb03
DESKTOP-JHDEJV9\bycho <bychoi00@mediasol.kr>
2018-03-01 09:56:22 +0900

    Revert "no message"
    This reverts commit 2d460c27e192a9639553e4c69ede5f76bab95df8.

--- bible_test/app/src/main/java/com/example/media_choi/bible_test/SplashScreen.java
+++ bible_test/app/src/main/java/com/example/media_choi/bible_test/SplashScreen.java
@@ -3,8 +3,6 @@ package com.example.media_choi.bible_test;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
@@ -14,25 +12,22 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+//
 
-        LogoLauncher logoLauncher = new LogoLauncher();
-        logoLauncher.start();
-    }
-
-    private class LogoLauncher extends Thread{
-        @Override
-        public void run() {
-            try{
-                sleep(1500);
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
-                finish();
-            }catch (InterruptedException e){
-                e.printStackTrace();
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
             }
-        }
+        };
+        thread.start();
     }
 }
