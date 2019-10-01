022783e7a0f52c4ab0476bddb946be41842d453a
bychoi00 <bychoisecond@gmail.com>
2019-10-02 00:45:11 +0900

    글씨 잘리는 부분 수정 (dp->sp), 글자크기 줄임 2.0버전으로 릴리즈

--- bible_test/app/src/main/java/com/bibleapp/media_choi/bible_test/MainActivity.java
+++ bible_test/app/src/main/java/com/bibleapp/media_choi/bible_test/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
